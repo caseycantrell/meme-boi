a3bccb9d47b886ebc7387be3c20a50d831663bb1
caseycantrell <cantrellcasey@gmail.com>
2022-01-13 15:26:49 -0600

    adds meme data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,21 @@
+import memeData from "../assets/memeData";
+
 export default function Meme () {
+
+    function getMemeImage() {
+        const memesArray = memeData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const { url } = memesArray[randomNumber];
+        console.log(url);
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form--input" />
                 <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--button">Get new image</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get new image</button>
+            </div>
         </main>
     )
 }
