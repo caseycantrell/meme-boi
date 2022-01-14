177ab1441fcf48675ecad6e3d68fe86df5cc40bb
caseycantrell <cantrellcasey@gmail.com>
2022-01-13 19:31:58 -0600

    adds useState to image fetching

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,34 @@
-import memeData from "../assets/memeData";
+import { useState } from "react";
+import memesData from "../assets/memesData";
 
 export default function Meme () {
 
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+
     function getMemeImage() {
-        const memesArray = memeData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const { url } = memesArray[randomNumber];
-        console.log(url);
-    }
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }));
+    };
 
     return (
         <main>
             <div className="form">
                 <input type="text" placeholder="Top text" className="form--input" />
                 <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--button" onClick={getMemeImage}>Get new image</button>
+                <button className="form--button" onClick={getMemeImage}>Get random image</button>
+                <img src={meme.randomImage} className="meme--image" />
             </div>
         </main>
-    )
-}
+    );
+};
