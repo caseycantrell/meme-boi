86b2854ab8f98a5e01a9008b789a04de806b899c
caseycantrell <cantrellcasey@gmail.com>
2022-01-14 20:49:48 -0600

    fixes image size problem

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -35,12 +35,13 @@ export default function Meme () {
                 <input type="text" placeholder="Top text" className="form--input" name="topText" value={meme.topText} onChange={handleChange} />
                 <input type="text" placeholder="Bottom text" className="form--input" name="bottomText" value={meme.bottomText} onChange={handleChange} />
                 <button className="form--button" onClick={getMemeImage}>Get random image</button>
-                <div className="meme">
+            </div>
+            <br />
+            <div className="meme">
                     <img src={meme.randomImage} className="meme--image" />
                     <h2 className="meme--text top">{meme.topText}</h2>
                     <h2 className="meme--text bottom">{meme.bottomText}</h2>
                 </div>
-            </div>
         </main>
     );
 };
