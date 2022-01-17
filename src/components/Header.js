c4efc673ff5f3805352dd36d1ded589a53f69fb3
caseycantrell <cantrellcasey@gmail.com>
2022-01-16 23:02:33 -0600

    tweaks styling a bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ function Header() {
             <header className='header'>
                 <img src="./img/meme-face.png" className="header--image" alt="header" />
                 <h1 className='header--name'>memeboi</h1>
-                {/* <h2 className='header--title'>make ur memes come true</h2> */}
             </header>
            
         </div>
